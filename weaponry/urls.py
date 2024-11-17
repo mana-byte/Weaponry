610e49ce7c71cca47efea6720b4df204c7613e2b
mana-byte <manaikilaut@gmail.com>
2024-11-17 16:06:53 +0100

    added(templates, views): form is now functional

--- weaponry/urls.py
+++ weaponry/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update_weapon/<str:pk>/', views.update_weapon, name='update_weapon'),
 ]
--- weaponry/views.py
+++ weaponry/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Weapon, Place
+from .forms import WeaponForm, PlaceForm
 
 
 def index(request):
     weapon = Weapon.objects.all()
     places = Place.objects.all()
     return render(request, 'index.html', {"weapons": weapon, "places": places})
+
+
+def update_weapon(request, pk):
+    weapon = get_object_or_404(Weapon, pk=pk)
+    print("works")
+    if request.method == "POST":
+        form = WeaponForm(request.POST, instance=weapon)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = WeaponForm(instance=weapon)
+    return render(request, 'update_weapon.html', {"form": form})
